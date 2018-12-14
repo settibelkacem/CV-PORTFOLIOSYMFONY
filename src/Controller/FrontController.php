b41555d280ebf956ea30a103baea5b468cb3f999
alegerieoran <settibelkacem313@gmail.com>
2018-12-14 10:49:53 +0100

    Mon site cv avec symfony

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/front", name="front")
+     */
+    public function index()
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(){
+        return $this->render('front/home.html.twig');
+    }
+}
